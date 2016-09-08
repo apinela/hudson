8eaabbaba4dc239e37d02fcdf28eb2e46c3f625f
André Pinela <sheffzor@gmail.com>
2016-09-08 04:03:38 +0100

    multi-config-builds: setup archive folder and set CLEAN in jenkins
    Change-Id: I651789edce54912cb346c733b31d2589a74178a0

--- multi-config-job.sh
+++ multi-config-job.sh
@@ -5,12 +5,13 @@ export VERSION=${Array[1]}
 export REPO_BRANCH=${ROM}-${VERSION}
 export REPO_SYNC=true
 export BUILD_TARGETS=otapackage
-export CLEAN=true
 export LUNCH=${ROM}_${DEVICE}-${TYPE}
 echo "LUCNH=$LUNCH - $VERSION"
 cd ../../../../../../
+ORI_WORKSPACE=$WORKSPACE
 export WORKSPACE=$(pwd)
 rm -rf hudson/ build_env/
 curl -ksO https://raw.githubusercontent.com/apinela/hudson/master/job.sh
 chmod a+x job.sh
 ./job.sh
+cp -Rf ../android/archive/ $ORI_WORKSPACE
