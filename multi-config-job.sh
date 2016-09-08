c29f704ea33d383c110bd4e2f2c199ce2b1e10c8
André Pinela <sheffzor@gmail.com>
2016-09-08 03:49:02 +0100

    multi-config-builds: fix workspace path
    Change-Id: Ifd15464574ccdc4dafc1581ecbd413debd889a8f

--- multi-config-job.sh
+++ multi-config-job.sh
@@ -7,6 +7,10 @@ export REPO_SYNC=true
 export BUILD_TARGETS=otapackage
 export CLEAN=true
 export LUNCH=${ROM}_${DEVICE}-${TYPE}
+echo "LUCNH=$LUNCH - $VERSION"
+cd ../../../../../../
+export WORKSPACE=$(pwd)
 rm -rf hudson/ build_env/
+curl -ksO https://raw.githubusercontent.com/apinela/hudson/master/job.sh
 chmod a+x job.sh
 ./job.sh
